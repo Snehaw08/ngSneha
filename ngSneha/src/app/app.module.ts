712bb60d80facb67ae08801c7abbf6a49c76e0aa
Snehaw08 <snehakshinde08@gmail.com>
2020-06-12 18:04:53 +0530

    if else switch pipes

--- ngSneha/src/app/app.module.ts
+++ ngSneha/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { SqrtPipe } from './pipes/sqrt.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SqrtPipe],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule{} 
--- ngSneha/src/main.ts
+++ ngSneha/src/main.ts
@@ -3,3 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+//Google serach below
+
+
+//spread and rest operator in escma
+//rxjs
+//isolates scope @ sign npmnscoping to handle children  
+//package json in ~ ^ sign used 
+//observable
