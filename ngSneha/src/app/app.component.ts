12a2feca85e9705168c16202262a463c0cb19053
Snehaw08 <snehakshinde08@gmail.com>
2020-06-13 23:57:40 +0530

    al-shah

--- ngSneha/src/app/app.component.ts
+++ ngSneha/src/app/app.component.ts
@@ -6,50 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  tittle = 'App';
-  arr = ['pune', 'mumbai', 'nashik'];
-
-  cars = [
-    {
-      name: 'Hyundai',
-      varient: 'Verna',
-      car_color: 'red',
-      year: 2018,
-      instock: 5,
-      country : 'South Korea',
-      prices : 650000,
-      canPurch: true,
-    },
-    {
-      name: 'Honda',
-      varient: 'City',
-      car_color: 'hotpink',
-      year: 2016,
-      instock: 0,
-      country: 'Japan',
-      prices: 700000,
-      canPurch: false,
-
-    },
-    {
-      name: 'Ford',
-      varient: 'ecosport',
-      car_color: 'green',
-      year: 2014,
-      instock: 3,
-      country: 'UK',
-      prices: 950000,
-      canPurch: true,
-    },
-    {
-      name: 'MarutiSuzuki',
-      varient: 'Baleno',
-      car_color: 'blue',
-      year: 2012,
-      instock: 1,
-      country: 'India',
-      prices: 450000,
-      canPurch: true,
-    },
-  ];
+  
 }
